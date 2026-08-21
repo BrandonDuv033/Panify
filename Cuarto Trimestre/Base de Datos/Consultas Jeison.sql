afd352aec226bed8d2b54e325e40cbf5a1c475b7
Jeison <diazacostajeison@gmail.com>
2026-08-21 07:50:09 -0500

    feat(sql): agregar dos nuevas subconsultas
    Se agregaron dos nuevas subconsultas a los entregables SQL del proyecto Panify, relacionadas con los requerimientos funcionales.

--- Cuarto Trimestre/Base de Datos/Consultas Jeison.sql
+++ Cuarto Trimestre/Base de Datos/Consultas Jeison.sql
@@ -42,3 +42,67 @@ LEFT JOIN domiciliario d
     ON u.domiciliario_idDomiciliario = d.idDomiciliario
 LEFT JOIN panadero p 
     ON u.panadero_idPanadero = p.idPanadero;
+    
+##3. Comparar stock con unidades solicitadas
+##Consultar los productos y determinar la diferencia entre el stock actual y la cantidad total de unidades solicitadas en los pedidos.
+
+SELECT
+    p.idProducto,
+    p.nombre,
+    i.stockActual,
+    i.stockMinimo,
+    COALESCE(t.totalSolicitado, 0) AS totalSolicitado,
+    (
+        i.stockActual - COALESCE(t.totalSolicitado, 0)
+    ) AS diferenciaStock
+FROM producto p
+
+LEFT JOIN inventario i
+    ON p.idProducto = i.producto_idProducto
+
+LEFT JOIN (
+    SELECT
+        dp.producto_idProducto,
+        SUM(dp.cantidad) AS totalSolicitado
+    FROM detalle_pedido dp
+
+    INNER JOIN pedido pe
+        ON dp.pedido_idPedido = pe.idPedido
+
+    INNER JOIN cliente c
+        ON pe.cliente_idCliente = c.idCliente
+
+    GROUP BY dp.producto_idProducto
+) t
+    ON p.idProducto = t.producto_idProducto;
+    
+##4. Promedio de compra de cada cliente
+##Consultar los clientes y calcular el valor promedio de los productos que han comprado, teniendo en cuenta la cantidad y el precio fijo.
+
+    SELECT
+    c.idCliente,
+    u.nombre,
+    u.apellido,
+    COALESCE(t.promedioCompra, 0) AS promedioCompra,
+    COALESCE(t.totalUnidades, 0) AS totalUnidades
+FROM cliente c
+
+INNER JOIN usuario u
+    ON u.cliente_idCliente = c.idCliente
+
+LEFT JOIN (
+    SELECT
+        pe.cliente_idCliente,
+        AVG(dp.cantidad * dp.precioFijo) AS promedioCompra,
+        SUM(dp.cantidad) AS totalUnidades
+    FROM pedido pe
+
+    INNER JOIN detalle_pedido dp
+        ON pe.idPedido = dp.pedido_idPedido
+
+    INNER JOIN producto p
+        ON dp.producto_idProducto = p.idProducto
+
+    GROUP BY pe.cliente_idCliente
+) t
+    ON c.idCliente = t.cliente_idCliente;
