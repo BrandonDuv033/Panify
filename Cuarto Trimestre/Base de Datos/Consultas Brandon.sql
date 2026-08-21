69286368996fa80d0966710e24974ad182768a9b
Brandon <brandonduv007@gmail.com>
2026-08-21 07:08:26 -0500

    docs: actualizar entregables del cuarto trimestre
    Se agrega la presentación correspondiente a los entregables del cuarto trimestre y se incorpora una nueva consulta SQL como parte de los entregables del proyecto Panify.

--- Cuarto Trimestre/Base de Datos/Consultas Brandon.sql
+++ Cuarto Trimestre/Base de Datos/Consultas Brandon.sql
@@ -19,3 +19,32 @@ INNER JOIN domiciliario d
 INNER JOIN usuario ud 
     ON d.idDomiciliario = ud.domiciliario_idDomiciliario;
     
+    
+# VISUALIZAR PEDIDOS PENDIENTES
+# Mostrar los pedidos registrados junto con el cliente que los realizó, 
+# el domiciliario encargado, la ruta de entrega y los productos incluidos en el pedido.
+
+SELECT 
+    pe.idPedido,
+    c.idCliente, u.nombre as Cliente,
+    d.idDomiciliario,
+    re.idRuta_entrega,
+    re.urlRutaGoogle,
+    pe.estadoPedido,
+    p.nombre AS producto,
+    dp.cantidad,
+    ROUND(dp.cantidad * dp.precioFijo, 2) AS subtotal
+FROM usuario u
+INNER JOIN cliente c
+on  u.cliente_idCliente = c.idCliente 
+INNER JOIN pedido pe
+    ON pe.cliente_idCliente = c.idCliente
+INNER JOIN domiciliario d
+    ON pe.domiciliario_idDomiciliario = d.idDomiciliario
+INNER JOIN ruta_entrega re
+    ON pe.ruta_entrega_idRuta_entrega = re.idRuta_entrega
+INNER JOIN detalle_pedido dp
+    ON pe.idPedido = dp.pedido_idPedido
+INNER JOIN producto p
+    ON dp.producto_idProducto = p.idProducto
+ORDER BY pe.idPedido;
