0c271b905dee1a5e31384c2c9f6bd22f167702f5
Julian01602 <julicahvez12@gmail.com>
2026-09-16 09:39:19 -0500

    ```text feat(recuperar): agrega ruta de recuperación  "incompleta "```

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/common/Navbar.jsx";
 import Hero from "./components/common/Hero.jsx";
 import ProductsCard from "./components/common/ProductsCard.jsx";
 import Footer from "./components/common/Footer.jsx";
 import ProtectedRoute from "./components/common/ProtectedRoute.jsx";
+
 import Dashboard from "./pages/admin/Dashboard.jsx";
+import RecuperarContrasena from "./components/recuperar.jsx";
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Hero />
       <ProductsCard />
+
       <Routes>
         <Route
           path="/dashboard"
@@ -20,9 +26,16 @@ function App() {
             </ProtectedRoute>
           }
         />
+
+        <Route
+          path="/recuperar"
+          element={<RecuperarContrasena />}
+        />
       </Routes>
+
       <Footer />
     </Router>
   );
 }
+
 export default App;
