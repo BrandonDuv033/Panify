2cbbd9fe310371a421fdca9d19df782ff83c8caa
Julian01602 <mcrmunoz25@gmail.com>
2026-09-18 08:02:52 -0500

    feat(cliente): transformar catalogo estatico a componente react
    Se migra la vista del catálogo de productos y el carrito de compras de HTML estático a un componente funcional de React con hooks de estado y efectos. Esto permite la gestión dinámica de los productos, el filtrado en tiempo real, el manejo del carrito de compras y la integración con la base de datos simulada mediante json-server.

--- frontend/src/components/common/Hero.jsx
+++ frontend/src/components/common/Hero.jsx
@@ -1,14 +1,19 @@
+
+import { Link } from 'react-router-dom';
+
 export default function Hero() {
   return (
     <section className="banner">
       <h1>Distribuidora Oro Pan</h1>
       <p>Horneamos Calidad, Entregamos Confianza</p>
-      <button
-        type="button"
-        className="btn btn-secondary"
+      
+      {/* Botón convertido en Link para navegar al catálogo/productos */}
+      <Link
+        to="/Pedidos"
+        className="btn btn-secondary text-decoration-none"
       >
         <i className="fa-solid fa-basket-shopping me-2"></i>Hacer Pedido
-      </button>
+      </Link>
     </section>
   );
 }
